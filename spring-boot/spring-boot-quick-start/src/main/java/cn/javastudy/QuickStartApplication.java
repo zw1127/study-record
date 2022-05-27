681ab50b7921dde680166070f1446549c67a0049
zw1127 <zw1016@gmail.com>
2022-05-27 10:55:58 +0800

    初始化项目，springboot quick start

---
+++ spring-boot/spring-boot-quick-start/src/main/java/cn/javastudy/QuickStartApplication.java
@@ -0,0 +1,21 @@
+package cn.javastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class QuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartApplication.class);
+    }
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+
+}
