5416e86b95f48794bb54ad91f5bc7819dfd3e240
wzhao6684 <wzhao6684@fiberhome.com>
2022-07-20 21:08:09 +0800

    exception handler

---
+++ spring-boot/spring-boot-exception/src/main/java/cn/javastudy/springboot/exception/handler/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package cn.javastudy.springboot.exception.handler;
+
+import cn.javastudy.springboot.exception.exception.UserNotExistException;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(UserNotExistException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> handleUserNotExistsException(UserNotExistException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", e.getId());
+        map.put("message", e.getMessage());
+        return map;
+    }
+}
---
+++ spring-boot/spring-boot-exception/src/test/java/cn/javastudy/springboot/exception/ExceptionApplicationTest.java
@@ -0,0 +1,16 @@
+package cn.javastudy.springboot.exception;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExceptionApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
