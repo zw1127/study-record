f92417b4d7c61ef4b746a6d437834c09d939510d
zw1127 <zw1016@gmail.com>
2023-01-17 14:29:28 +0800

    线程池测试

--- spring-boot/spring-boot-ldap/src/test/java/cn/javastudy/springboot/ldap/LdapApplicationTest.java
+++ spring-boot/spring-boot-ldap/src/test/java/cn/javastudy/springboot/ldap/LdapApplicationTest.java
@@ -2,6 +2,9 @@ package cn.javastudy.springboot.ldap;
 
 import cn.javastudy.springboot.ldap.domain.Person;
 import cn.javastudy.springboot.ldap.repo.PersonRepository;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +33,32 @@ public class LdapApplicationTest {
         personRepository.save(person);
 
         personRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    public void testThreadWait() throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        service.submit(() -> process(1000, "job1"));
+        service.submit(() -> process(2000, "job2"));
+        service.submit(() -> process(10000, "job3"));
+        service.submit(() -> process(1000, "job4"));
+
+        service.awaitTermination(6, TimeUnit.SECONDS);
+    }
+
+    private void process(long timeout, String jobName) {
+        System.out.println("begin to process thread:" + jobName + " current: " + System.currentTimeMillis());
+        Object obj = new Object();
+        try {
+            synchronized (obj) {
+                obj.wait(timeout);
+            }
+        } catch (Throwable throwable) {
+            System.out.println("interrupted.");
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("end processed thread:" + jobName + " current: " + System.currentTimeMillis());
     }
 }
