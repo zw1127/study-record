3e866663221f4c81e073ef68c8ae251672a8daff
zw1127 <zw1016@gmail.com>
2023-07-12 14:05:11 +0800

    测试ssh keepalive

--- spring-boot-netconf-simulator/src/test/java/cn/javastudy/springboot/simulator/netconf/service/impl/SshClientHeartbeatTest.java
+++ spring-boot-netconf-simulator/src/test/java/cn/javastudy/springboot/simulator/netconf/service/impl/SshClientHeartbeatTest.java
@@ -4,16 +4,20 @@ import static com.google.common.base.Verify.verify;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.opendaylight.netconf.nettyutil.handler.ssh.authentication.LoginPasswordHandler;
 import org.opendaylight.netconf.nettyutil.handler.ssh.client.NetconfClientBuilder;
 import org.opendaylight.netconf.nettyutil.handler.ssh.client.NetconfSshClient;
 import org.opendaylight.netconf.nettyutil.handler.ssh.client.NettyAwareClientSession;
 import org.opendaylight.netconf.shaded.sshd.client.future.AuthFuture;
+import org.opendaylight.netconf.shaded.sshd.common.PropertyResolverUtils;
+import org.opendaylight.netconf.shaded.sshd.common.io.BuiltinIoServiceFactoryFactories;
 import org.opendaylight.netconf.shaded.sshd.core.CoreModuleProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Ignore
 public class SshClientHeartbeatTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(SshClientHeartbeatTest.class);
@@ -24,21 +28,27 @@ public class SshClientHeartbeatTest {
     public void testHeartbeat() throws Exception {
         NetconfSshClient sshClient = new NetconfClientBuilder().build();
 
+        sshClient.setIoServiceFactoryFactory(BuiltinIoServiceFactoryFactories.NETTY.create());
+
         // Disable default timeouts from mina sshd
         sshClient.getProperties().put(CoreModuleProperties.AUTH_TIMEOUT.getName(), "0");
-        sshClient.getProperties().put(CoreModuleProperties.IDLE_TIMEOUT.getName(), "10000");
-        sshClient.getProperties().put(CoreModuleProperties.NIO2_READ_TIMEOUT.getName(), "0");
+        sshClient.getProperties().put(CoreModuleProperties.IDLE_TIMEOUT.getName(), "0");
+        sshClient.getProperties().put(CoreModuleProperties.NIO2_READ_TIMEOUT.getName(), "30000");
         sshClient.getProperties().put(CoreModuleProperties.TCP_NODELAY.getName(), true);
-        sshClient.getProperties().put(CoreModuleProperties.HEARTBEAT_INTERVAL.getName(), "1000");
+//        sshClient.getProperties().put(CoreModuleProperties.HEARTBEAT_INTERVAL.getName(), "5000");
+
+        PropertyResolverUtils.updateProperty(sshClient, "heartbeat-interval", 5000);
+        PropertyResolverUtils.updateProperty(sshClient, "heartbeat-reply-wait", 50000);
+        PropertyResolverUtils.updateProperty(sshClient, "heartbeat-request", "keepalive@openssh.com");
 
         sshClient.setNioWorkers(SSH_DEFAULT_NIO_WORKERS);
         sshClient.start();
 
-        String username = "hp";
-        LoginPasswordHandler authenticationHandler = new LoginPasswordHandler(username, "Fiberhome.2022");
+        String username = "admin";
+        LoginPasswordHandler authenticationHandler = new LoginPasswordHandler(username, "Admin_123");
 
-        sshClient.connect(authenticationHandler.getUsername(), "10.190.49.224", 22)
-            .verify(5, TimeUnit.SECONDS)
+        sshClient.connect(authenticationHandler.getUsername(), "10.190.23.232", 848)
+            .verify(15, TimeUnit.SECONDS)
             .addListener(connectFuture -> {
                 final var clientSession = connectFuture.getSession();
                 verify(clientSession instanceof NettyAwareClientSession, "Unexpected session %s", clientSession);
@@ -62,6 +72,7 @@ public class SshClientHeartbeatTest {
 
                     LOG.info("open ssh successful.");
                 });
-            }).wait();
+            });
+        TimeUnit.DAYS.sleep(1);
     }
 }
