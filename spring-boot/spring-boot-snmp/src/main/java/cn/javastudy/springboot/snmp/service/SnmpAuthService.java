b461621ce0afed7d244e5223b73b90231b2d1625
zw1127 <zw1016@gmail.com>
2023-08-28 15:13:49 +0800

    snmpv3
    完善snmp4j关于snmpv3相关缓存的处理

--- spring-boot/spring-boot-snmp/src/main/java/cn/javastudy/springboot/snmp/service/SnmpAuthService.java
+++ spring-boot/spring-boot-snmp/src/main/java/cn/javastudy/springboot/snmp/service/SnmpAuthService.java
@@ -10,22 +10,29 @@ import cn.javastudy.springboot.snmp.data.SnmpProtocolVersion;
 import cn.javastudy.springboot.snmp.data.SnmpTransportConfiguration;
 import java.util.Optional;
 import javax.annotation.Resource;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.snmp4j.AbstractTarget;
 import org.snmp4j.CommunityTarget;
 import org.snmp4j.Target;
 import org.snmp4j.UserTarget;
 import org.snmp4j.security.SecurityLevel;
 import org.snmp4j.security.SecurityModel;
+import org.snmp4j.security.SecurityProtocols;
 import org.snmp4j.security.USM;
 import org.snmp4j.security.UsmUser;
 import org.snmp4j.smi.GenericAddress;
+import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.UdpAddress;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SnmpAuthService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SnmpAuthService.class);
 
     @Resource
     private SnmpTransportService snmpTransportService;
@@ -60,30 +67,38 @@ public class SnmpAuthService {
                 target = communityTargetV2;
                 break;
             case V3:
-                OctetString username = new OctetString(transportConfig.getUsername());
                 OctetString securityName = new OctetString(transportConfig.getSecurityName());
-
-                OctetString engineId;
-                if (StringUtils.isNotEmpty(transportConfig.getEngineId())) {
-                    engineId = new OctetString(transportConfig.getEngineId());
-                } else {
-                    byte[] targetEngineID = snmpTransportService.getSnmp().discoverAuthoritativeEngineID(address, 1000);
-                    engineId = new OctetString(targetEngineID);
-                }
-
-                UsmUser usmUser = buildUsmUser(transportConfig);
-                USM usm = new USM();
-                if (usm.hasUser(engineId, securityName)) {
-                    usm.removeAllUsers(username, engineId);
-                }
-                usm.addUser(usmUser);
 
                 UserTarget<UdpAddress> userTarget = new UserTarget<>();
 
-                userTarget.setAuthoritativeEngineID(engineId.getValue());
                 userTarget.setSecurityName(securityName);
                 userTarget.setSecurityModel(SecurityModel.SECURITY_MODEL_USM);
                 userTarget.setSecurityLevel(SecurityLevel.AUTH_PRIV);
+
+                String engineId = transportConfig.getEngineId();
+                if (StringUtils.isEmpty(engineId)) {
+                    byte[] authoritativeEngineID =
+                        snmpTransportService.getSnmp().discoverAuthoritativeEngineID(address, 1000);
+                    requireNonNull(authoritativeEngineID, address + " discoverAuthoritativeEngineID is null");
+                    userTarget.setAuthoritativeEngineID(authoritativeEngineID);
+                } else {
+                    OctetString authoritativeEngineID = resolveEngineId(engineId);
+                    userTarget.setAuthoritativeEngineID(authoritativeEngineID.getValue());
+                }
+                byte[] authoritativeEngineID = userTarget.getAuthoritativeEngineID();
+
+                AuthenticationProtocol authenticationProtocol = transportConfig.getAuthenticationProtocol();
+                requireNonNull(authenticationProtocol, "AuthenticationProtocol is null");
+
+                PrivacyProtocol privacyProtocol = transportConfig.getPrivacyProtocol();
+                requireNonNull(privacyProtocol, "PrivacyProtocol is null");
+
+                int securityLevel = resolveSecurityLevel(authenticationProtocol, privacyProtocol);
+                userTarget.setSecurityLevel(securityLevel);
+
+                USM usm = snmpTransportService.getSnmp().getUSM();
+                addUsmUser(usm, transportConfig, authoritativeEngineID);
+
                 target = userTarget;
                 break;
             default:
@@ -96,6 +111,32 @@ public class SnmpAuthService {
         target.setVersion(protocolVersion.getCode());
 
         return target;
+    }
+
+    private static int resolveSecurityLevel(AuthenticationProtocol authProtocol, PrivacyProtocol privProtocol) {
+        if (PrivacyProtocol.NONE.equals(privProtocol)) {
+            if (AuthenticationProtocol.NONE.equals(authProtocol)) {
+                return SecurityLevel.noAuthNoPriv.getSnmpValue();
+            } else {
+                return SecurityLevel.authNoPriv.getSnmpValue();
+            }
+        } else {
+            if (!AuthenticationProtocol.NONE.equals(authProtocol)) {
+                return SecurityLevel.authPriv.getSnmpValue();
+            }
+        }
+
+        LOG.warn("AuthenticationProtocol is:{}, PrivacyProtocol is:{}", authProtocol, privProtocol);
+        return SecurityLevel.undefined.getSnmpValue();
+    }
+
+    public static OctetString resolveEngineId(String engineId) {
+        char delimiter = ':';
+        if (StringUtils.contains(engineId, delimiter)) {
+            return OctetString.fromHexString(engineId, delimiter);
+        }
+
+        return OctetString.fromHexStringPairs(engineId);
     }
 
     private static UsmUser buildUsmUser(SnmpTransportConfiguration transportConfig) {
@@ -114,6 +155,69 @@ public class SnmpAuthService {
             OctetString.fromString(transportConfig.getAuthenticationPassphrase()),
             privacyProtocol.getOid(),
             OctetString.fromString(transportConfig.getPrivacyPassphrase()));
+    }
+
+    public static void addUsmUser(USM usm, SnmpTransportConfiguration param, byte[] authoritativeEngineID) {
+        String securityUser = requireNonNull(param.getSecurityName(), "SNMP v3 SecurityUser is null");
+        OctetString securityName = new OctetString(securityUser);
+
+        AuthenticationProtocol authenticationProtocol = param.getAuthenticationProtocol();
+        requireNonNull(authenticationProtocol, "AuthenticationProtocol is null");
+
+        PrivacyProtocol privacyProtocol = param.getPrivacyProtocol();
+        requireNonNull(privacyProtocol, "PrivacyProtocol is null");
+
+        OID authProtocolOid = authenticationProtocol.getOid();
+        OID privProtocolOid = privacyProtocol.getOid();
+
+        OctetString authPassphrase = OctetString.fromString(param.getAuthenticationPassphrase());
+        authPassphrase = passwordToKey(authProtocolOid, authPassphrase, authoritativeEngineID);
+
+        OctetString privPassphrase = OctetString.fromString(param.getPrivacyPassphrase());
+        privPassphrase = passwordToKey(privProtocolOid, authProtocolOid, privPassphrase, authoritativeEngineID);
+
+        OctetString engineId = OctetString.fromByteArray(authoritativeEngineID);
+        if (usm.hasUser(engineId, securityName)) {
+            usm.removeAllUsers(securityName, engineId);
+        }
+
+        usm.addLocalizedUser(
+            authoritativeEngineID, securityName,
+            authProtocolOid, authPassphrase.getValue(),
+            privProtocolOid, privPassphrase.getValue()
+        );
+    }
+
+    private static OctetString passwordToKey(OID authProtocolID, OctetString passwordString, byte[] engineID) {
+        if (authProtocolID == null || passwordString == null || passwordString.length() == 0) {
+            return new OctetString();
+        }
+
+        if (ArrayUtils.isEmpty(engineID)) {
+            return passwordString;
+        }
+
+        return OctetString.fromByteArray(SecurityProtocols.getInstance().passwordToKey(
+            authProtocolID, passwordString, engineID));
+    }
+
+    private static OctetString passwordToKey(OID privProtocolID,
+                                             OID authProtocolID,
+                                             OctetString passwordString,
+                                             byte[] engineID) {
+        if (privProtocolID == null
+            || authProtocolID == null
+            || passwordString == null
+            || passwordString.length() == 0) {
+            return new OctetString();
+        }
+
+        if (ArrayUtils.isEmpty(engineID)) {
+            return passwordString;
+        }
+
+        return OctetString.fromByteArray(SecurityProtocols.getInstance().passwordToKey(
+            privProtocolID, authProtocolID, passwordString, engineID));
     }
 
 //    public void cleanUpSnmpAuthInfo(DeviceSessionContext sessionContext) {
