15131331b38436597a193f9bb9513cbcf74f9ae4
zw1127 <zw1016@gmail.com>
2023-11-22 13:35:29 +0800

    mybatis generator

--- spring-boot/spring-boot-aspect/src/test/java/cn/javastudy/springboot/aspect/RateLimiterTest.java
+++ spring-boot/spring-boot-aspect/src/test/java/cn/javastudy/springboot/aspect/RateLimiterTest.java
@@ -1,6 +1,11 @@
 package cn.javastudy.springboot.aspect;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.RateLimiter;
+import com.google.common.util.concurrent.SettableFuture;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
@@ -22,4 +27,30 @@ public class RateLimiterTest {
         System.out.println("cost:" + Duration.between(started, Instant.now()).toMillis());
     }
 
+
+    @Test
+    public void testFuturesAddCallback() throws Exception {
+        Futures.addCallback(futures(), new FutureCallback<>() {
+            @Override
+            public void onSuccess(Integer result) {
+                System.out.println("test success");
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println("test failed");
+            }
+        }, MoreExecutors.directExecutor());
+        TimeUnit.SECONDS.sleep(30);
+    }
+
+    private ListenableFuture<Integer> futures() {
+        SettableFuture<Integer> future = SettableFuture.create();
+        future.set(throwRuntimeException());
+        return future;
+    }
+
+    private Integer throwRuntimeException() {
+        throw new RuntimeException("test");
+    }
 }
