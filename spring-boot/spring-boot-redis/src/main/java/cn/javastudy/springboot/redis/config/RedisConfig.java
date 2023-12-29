12264efae9480db37f7021cb510882c1e7f6a84e
zw1127 <zw1016@gmail.com>
2023-12-29 13:42:37 +0800

    去掉无用的导入

--- spring-boot/spring-boot-redis/src/main/java/cn/javastudy/springboot/redis/config/RedisConfig.java
+++ spring-boot/spring-boot-redis/src/main/java/cn/javastudy/springboot/redis/config/RedisConfig.java
@@ -1,21 +1,11 @@
 package cn.javastudy.springboot.redis.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * redis配置
